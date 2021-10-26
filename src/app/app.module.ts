68f718041342065b14415a9b1bf921155c4c882e
pavlencot <pavlenco.tanya@gmail.com>
2021-10-26 16:33:57 +0300

    Some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,44 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDatepickerModule} from '@angular/material/datepicker';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatRadioModule } from '@angular/material/radio';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FormComponent } from './views/booking/form.component';
-import { MatNativeDateModule } from '@angular/material/core';
 import { SigninComponent } from './views/signin/signin.component';
 import { RegistrationComponent } from './views/registration/registration.component';
 import { OrganizerProfileComponent } from './views/profile/organizer/organizer-profile/organizer-profile.component';
-import { PilotRegistrationComponent } from './views/registrations/pilot-registration/pilot-registration.component';
-import { OrganizerRegistrationComponent } from './views/registrations/organizer-registration/organizer-registration.component';
 import { PilotInstructorProfileComponent } from './views/profile/pilot-instructor-profile/pilot-instructor-profile.component';
 import { PilotProfileComponent } from './views/profile/pilot-profile/pilot-profile.component';
 import { HomePageComponent } from './views/home/home-page.component';
-import { CompetitionTableComponent } from './shared/competition-table/competition-table.component';
 import { CompetitionsPageComponent } from './views/competitions/competitions-page.component';
-// import { environment } from 'src/environments/environment';
 import { OrganizerCompetitionsComponent } from './views/profile/organizer/organizer-competitions/organizer-competitions.component';
 import { OrganizerEditComponent } from './views/profile/organizer/organizer-edit/organizer-edit.component';
 import { PaginatedTableComponent } from './shared/paginated-table/paginated-table.component';
-
 
 
 @NgModule({
@@ -55,12 +33,9 @@ import { PaginatedTableComponent } from './shared/paginated-table/paginated-tabl
     SigninComponent,
     RegistrationComponent,
     OrganizerProfileComponent,
-    PilotRegistrationComponent,
-    OrganizerRegistrationComponent,
     PilotInstructorProfileComponent,
     PilotProfileComponent,
     HomePageComponent,
-    CompetitionTableComponent,
     CompetitionsPageComponent,
     OrganizerCompetitionsComponent,
     OrganizerEditComponent,
